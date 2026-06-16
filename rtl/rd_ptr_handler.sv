c1392df0c2ef1a9273a4a7f7d628a59ef803d3c9
Chaitanya-jonnadula <chaitanyajonnadula21@gmail.com>
2026-06-16 21:34:51 +0530

    rd_ptr_handler.sv

--- rtl/rd_ptr_handler.sv
+++ rtl/rd_ptr_handler.sv
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.06.2026 17:47:23
-// Design Name: 
-// Module Name: rd_ptr_handler
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module rd_ptr_handler #(
     parameter PTR_WIDTH = 4
 )(
